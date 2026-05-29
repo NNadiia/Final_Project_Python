a16333e475755050ff1cca3dfc0251ef50857740
NNadiia <nadiianazarchuk@gmail.com>
2026-05-29 13:41:07 +0200

    Added new queries in analytics.py with and updated  main.py to run analytics

---
+++ analytics.py
@@ -0,0 +1,58 @@
+import sqlite3
+from db import DB_NAME
+
+# SQL queries to calculate key business metrics:
+
+#Total revenue by source
+def get_revenue_by_source():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT source, ROUND(SUM(amount), 2) AS total_revenue, COUNT(*) AS total_orders "
+            "FROM EcomOrders GROUP BY source ORDER BY total_revenue DESC")
+        result = cursor.fetchall()
+    print("Revenue by source", result)
+    return result
+
+#Orders and revenue by month and source
+def get_orders_by_month():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT STRFTIME('%Y-%m', create_date) AS month, source, COUNT(*) AS total_orders, ROUND(SUM(amount), 2) AS total_revenue "
+        "FROM EcomOrders GROUP BY month, source ORDER BY month")
+        result = cursor.fetchall()
+    print("Orders and revenue by month and source", result)
+    return result
+    
+
+#Top 5 products by revenue and source
+def get_top_products():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT d.title, o.source, SUM(d.quantity) AS total_qty, ROUND(SUM(d.total_price), 2) AS total_revenue " 
+        "FROM EcomOrderDetails d JOIN EcomOrders o ON d.order_id = o.original_id " 
+        "GROUP BY d.title, o.source " 
+        "ORDER BY total_revenue DESC LIMIT 5")
+        result = cursor.fetchall()
+    print("Top 5 products by revenue and source", result)
+    return result
+    
+#Orders by financial status and source
+def get_orders_by_status():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT status_financial, source, COUNT(*) AS total_orders, ROUND(SUM(amount), 2) AS total_revenue " 
+        " FROM EcomOrders GROUP BY status_financial, source ORDER BY total_orders DESC")
+        result = cursor.fetchall()
+    print("Orders by financial status and source", result)
+    return result
+    
+#Total summary by sourc
+def get_total_summary():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT source, COUNT(*) AS total_orders, ROUND(SUM(amount), 2) AS total_revenue, " 
+        "ROUND(AVG(amount), 2) AS avg_order_value, SUM(total_quantity) AS total_items FROM EcomOrders GROUP BY source")
+        result = cursor.fetchall()
+    print("Total summary by source", result)
+    return result
+    
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from etl import run_etl
+from analytics import get_revenue_by_source, get_orders_by_month, get_top_products, get_total_summary, get_orders_by_status
 
 if __name__ == "__main__":   #runs the full data integration process with json data files
     run_etl( shopify_file="Shopify_05_26.txt", ebay_file="Ebay_26_05_26.txt")
+
+    get_revenue_by_source()
+    get_orders_by_month()
+    get_top_products()
+    get_orders_by_status()
+    get_total_summary()
+
