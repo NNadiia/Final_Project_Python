1136de7a356265328a4935ea776dd81a1f3b3f51
NNadiia <nadiianazarchuk@gmail.com>
2026-05-30 13:12:56 +0200

    Complete charts.py: interactive dashboard with RadioButtons filter by source and added currency

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from etl import run_etl
 from analytics import get_revenue_by_source, get_orders_by_month, get_top_products, get_total_summary, get_orders_by_status
+from charts import     plot_dashboard
+#from charts import plot_revenue_by_source, plot_orders_by_month, plot_top_products, plot_orders_by_status
+
 
 if __name__ == "__main__":   #runs the full data integration process with json data files
     run_etl( shopify_file="Shopify_05_26.txt", ebay_file="Ebay_26_05_26.txt")
@@ -10,3 +13,4 @@ if __name__ == "__main__":   #runs the full data integration process with json d
     get_orders_by_status()
     get_total_summary()
 
+    plot_dashboard()
